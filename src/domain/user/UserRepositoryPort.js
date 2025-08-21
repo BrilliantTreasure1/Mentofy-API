fb832332534773f50fad1c423330b9a33eabd4c1
BrilliantTreasure1 <brillianttrussure@gmail.com>
2025-08-21 11:18:56 +0330

    feat : adding UserRepositoryPort in domain/user

---
+++ src/domain/user/UserRepositoryPort.js
@@ -0,0 +1,12 @@
+class UserRepositoryPort {
+    async create(user) {
+      throw new Error("Method not implemented");
+    }
+  
+    async findByEmail(email) {
+      throw new Error("Method not implemented");
+    }
+  }
+  
+  module.exports = UserRepositoryPort;
+  
